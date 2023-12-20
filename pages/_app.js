9fbc2844c469fd8caa4035436dd2d6a0cc65aaf9
Luciano Souza <lucianorodriguess101@gmail.com>
2023-12-20 17:34:08 -0300

    Configure Material-UI in the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from '../src/theme'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Anunx</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />        
+      </ThemeProvider>
+    </React.Fragment>
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../src/theme'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
