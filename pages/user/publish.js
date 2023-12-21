7248565a2d7386cee44337252178d676b4ef6afd
Luciano Souza <lucianorodriguess101@gmail.com>
2023-12-21 16:29:33 -0300

    style: add HTML and CSS for image display in Publish page

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -6,10 +6,13 @@ import {
     Select,
     Button,  
     TextField,
+    IconButton,
 } from '@mui/material'
 
 import TemplateDefault from '../../src/templates/Default'
+import { DeleteForever } from '@mui/icons-material'
 
+import style from './publish.module.css'
 
 const StyledContainerTitle = styled(Container)(({ theme }) => ({
     padding: theme.spacing(8, 0, 6),
@@ -21,6 +24,24 @@ const StyledBox = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.background.white,
     padding: theme.spacing(3),
     paddingBottom: theme.spacing(4),
+}))
+const StyledBoxThumbsContainer = styled(Box)(() => ({
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: 15,
+}))
+const StyledBoxDropzone = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+    width: 186,
+    height: 137,
+    margin: '0 15px 15px 0',
+    backgroundColor: theme.palette.background.default,
+    border: '2px dashed black',
+    cursor: 'pointer',
 }))
 
 const Publish = () => {
@@ -88,6 +109,28 @@ const Publish = () => {
                     <Typography component="div" variant="body2" color="textPrimary">
                         A primeira imagem é a foto principal do seu anúncio.
                     </Typography>
+                    <StyledBoxThumbsContainer>
+                        <StyledBoxDropzone>
+                            <Typography variant="body2" color="textPrimary">
+                                Clique para adicionar ou arraste a imagem para aqui.
+                            </Typography>
+                        </StyledBoxDropzone>
+                        <Box
+                            className={style.boxThumb}
+                            style={{ backgroundImage: 'url(https://source.unsplash.com/random)' }}
+                        >
+                            <Box className={style.boxMainImage}>
+                                <Typography variant="body2" color="secondary">
+                                    Principal
+                                </Typography>
+                            </Box>
+                            <Box className={style.boxThumbMask}>
+                                <IconButton color="secondary">
+                                    <DeleteForever fontSize="large" />
+                                </IconButton>
+                            </Box>
+                        </Box>
+                    </StyledBoxThumbsContainer>
                 </StyledBox>
             </StyledContainerBox>
 
