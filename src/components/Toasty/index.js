96fc017345a502f27a0deb1c29b4df62aaa2613b
Luciano Souza <lucianorodriguess101@gmail.com>
2023-12-24 17:24:29 -0300

    feat: create component Toasty

---
+++ src/components/Toasty/index.js
@@ -0,0 +1,34 @@
+import {
+    Alert,
+    Snackbar,
+} from '@mui/material'
+
+
+const Toasty = ({ open, text, severity, onClose=null }) => {
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return
+        }
+
+        if(onClose) onClose()
+    }
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+        >
+            <Alert elevation={6} variant="filled" severity={severity}>
+                {text}
+            </Alert>
+        </Snackbar>
+    )
+}
+
+export default Toasty
