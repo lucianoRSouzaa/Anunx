d3d52d66217382e0b07e4c1e25ea25443cf6a541
Luciano Souza <lucianorodriguess101@gmail.com>
2023-12-29 10:24:25 -0300

    feat(user-dashboard): add confirmation dialog for removing advertisements

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import axios from 'axios'
 import { 
   Container,
   Typography,
@@ -12,6 +14,9 @@ import ProductsModel from '@/src/models/products'
 import TemplateDefault from '../../src/templates/Default'
 import Card from '../../src/components/Card'
 import { formatCurrency } from '../../src/utils/currency'
+import AlertDialog from '@/src/components/AlertDialog'
+import useToasty from '@/src/contexts/Toasty'
+import Link from 'next/link'
 
 const StyledButtonAdd = styled(Button)(() => ({
   margin: '30px auto',
@@ -19,8 +24,48 @@ const StyledButtonAdd = styled(Button)(() => ({
 }))
 
 const Home = ({ products }) => {
+  const { setToasty } = useToasty()
 
-  console.log(products)
+  const [openDialog, setOpenDialog] = useState(false)
+  const [productIdToRemove, setProductIdToRemove] = useState(null)
+  const [removedProducts, setRemovedProducts] = useState([])
+
+  const handleCloseDialog = () => setOpenDialog(false)
+
+  const handleClickOpenDialog = productId => {
+    setOpenDialog(true)
+    setProductIdToRemove(productId)
+  }
+
+  const handleConfirmRemove = () => {
+    axios.delete('/api/products/delete', {
+      data: {
+        id: productIdToRemove
+      }
+    })
+      .then(handleSuccess)
+      .catch(handleError)
+  }
+
+  const handleSuccess = () => {    
+    setOpenDialog(false)
+    setRemovedProducts([ ...removedProducts, productIdToRemove ])
+
+    setToasty({
+      open: true,
+      severity: 'success',
+      text: 'Anúncio removido com sucesso!'
+    })
+  }
+
+  const handleError = () => {
+    setOpenDialog(false)
+    setToasty({
+      open: true,
+      severity: 'error',
+      text: 'Ops, ocorreu um erro!'
+    })
+  }
 
   return (
     <TemplateDefault>
@@ -28,35 +73,58 @@ const Home = ({ products }) => {
         <Typography component="h1" variant="h2" align="center" sx={{ fontSize: '3.3em' }}>
             Meus Anúncios
         </Typography>
-        <StyledButtonAdd variant="contained" color="primary">
-          Publicar novo anúncio
-        </StyledButtonAdd>
+        <Link href="/user/publish" passHref style={{ textDecoration: 'none' }}>
+          <StyledButtonAdd variant="contained" color="primary">
+            Publicar novo anúncio
+          </StyledButtonAdd>
+        </Link>
       </Container>
       <Container maxWidth="lg">
+
+        {
+          products.length === 0 && 
+            <Typography component="div" variant="body1" align="center" color="textPrimary" gutterBottom>
+              Nenhum anúncio publicado
+            </Typography>
+        }
+
         <Grid container spacing={4}>
           {
-            products.map(product => (
-              <Grid item xs={12} sm={6} md={4}>
-                <Card
-                  image={`/uploads/${product.files[0].name}`}
-                  title={product.title}
-                  subtitle={formatCurrency(product.price)}
-                  actions={
-                    <>
-                      <Button size="small" color="primary">
-                        Editar
-                      </Button>
-                      <Button size="small" color="primary">
-                        Remover
-                      </Button>
-                    </>
-                  }
-                />
-              </Grid>
-            ))
+            products.map(product => {
+              if(removedProducts.includes(product._id)) return null
+
+              return (
+                <Grid key={product._id} item xs={12} sm={6} md={4}>
+                  <Card
+                    image={`/uploads/${product.files[0].name}`}
+                    title={product.title}
+                    subtitle={formatCurrency(product.price)}
+                    actions={
+                      <>
+                        <Button size="small" color="primary">
+                          Editar
+                        </Button>
+                        <Button size="small" color="primary" onClick={() => handleClickOpenDialog(product._id)}>
+                          Remover
+                        </Button>
+                      </>
+                    }
+                  />
+                </Grid>
+              )
+            })
           }
         </Grid>
       </Container>
+      <AlertDialog 
+        open={openDialog}
+        handleClose={handleCloseDialog}
+        handleConfirm={handleConfirmRemove}
+        title="Tem certeza que deseja remover o anúncio?"
+        message="Ao confirmar a operação, não poderá recuperar o anúncio."
+        confirmMsg = "Remover"
+        cancelMsg = "Cancelar"
+      />
     </TemplateDefault>
   )
 }
